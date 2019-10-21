910330872fde7d9ffaa03f5d5218292ff3b4e94e
Warvito <ianonimato@hotmail.com>
2019-10-21 14:51:42 +0100

    Fix bootstrap for bootstrap .63+

--- classifier_bootstrap_train.py
+++ classifier_bootstrap_train.py
@@ -3,6 +3,7 @@ from pathlib import Path
 import random as rn
 
 import numpy as np
+import pandas as pd
 from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.preprocessing import RobustScaler
@@ -17,13 +18,13 @@ def main():
     # ----------------------------------------------------------------------------
     n_bootstrap = 1000
     experiment_name = 'biobank_scanner1'
-    dataset_name = 'ADNI'
+    dataset_name = 'FBF_Brescia'
 
     participants_path = PROJECT_ROOT / 'data' / 'datasets' / dataset_name / 'participants.tsv'
     freesurfer_path = PROJECT_ROOT / 'data' / 'datasets' / dataset_name / 'freesurferData.csv'
 
     hc_label = 1
-    disease_label = 17
+    disease_label = 18
 
     # ----------------------------------------------------------------------------
     # Set random seed
@@ -35,6 +36,8 @@ def main():
     classifier_dataset_dir = classifier_dir / dataset_name
     classifier_dataset_analysis_dir = classifier_dataset_dir / '{:02d}_vs_{:02d}'.format(hc_label, disease_label)
     ids_dir = classifier_dataset_analysis_dir / 'ids'
+    predictions_dir = classifier_dataset_analysis_dir / 'predictions'
+    predictions_dir.mkdir(exist_ok=True)
     auc_bootstrap_train = []
     auc_bootstrap_test = []
     # ----------------------------------------------------------------------------
@@ -81,13 +84,17 @@ def main():
 
         best_svm = gridsearch.best_estimator_
 
-        predictions = best_svm.predict_proba(x_data)
+        pred = best_svm.predict(x_data)
+        predictions_proba = best_svm.predict_proba(x_data)
 
-        auc = roc_auc_score(y_data, predictions[:, 1])
+        auc = roc_auc_score(y_data, predictions_proba[:, 1])
 
         auc_bootstrap_train.append(auc)
 
         print('AUC = {:.03f}'.format(auc))
+
+        predictions_df = dataset_df[['Image_ID']]
+        predictions_df['predictions'] = pred
 
         # -----------------------------------------------------------------
         ids_filename_test = 'homogeneous_bootstrap_{:03d}_test.csv'.format(i_bootstrap)
@@ -110,13 +117,21 @@ def main():
 
         x_test = scaler.transform(x_test)
 
-        predictions = best_svm.predict_proba(x_test)
+        pred = best_svm.predict(x_test)
+        predictions_proba = best_svm.predict_proba(x_test)
 
-        auc = roc_auc_score(y_test, predictions[:, 1])
+        auc = roc_auc_score(y_test, predictions_proba[:, 1])
 
         auc_bootstrap_test.append(auc)
 
         print('AUC = {:.03f}'.format(auc))
+
+
+        temp = dataset_df[['Image_ID']]
+        temp['predictions'] = pred
+
+        predictions_df = pd.concat([predictions_df, temp], axis=0)
+        predictions_df.to_csv(predictions_dir / 'homogeneous_bootstrap_{:03d}_prediction.csv'.format(i_bootstrap), index=False)
 
     np.save(classifier_dataset_analysis_dir / 'aucs_train.npy', np.array(auc_bootstrap_train))
     np.save(classifier_dataset_analysis_dir / 'aucs_test.npy', np.array(auc_bootstrap_test))
